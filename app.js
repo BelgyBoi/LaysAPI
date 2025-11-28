d35d1d86fd9eab4948b65f97d07e1d4d73988910
Captain Jack Sparrow <tibo.kooreman.dev@gmail.com>
2025-11-28 16:29:38 +0100

    Working routes
    - adjusted routes placement
    - added the export (forgot it like a proper muppet)
    - tested everything and we're green to start building

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const port = 3000;
 app.use(express.json());
 
 const defaultRoutes = require('./routes/v1/default');
-const adminRoutes = require('./routes/v1/admin')
+const adminRoutes = require('./routes/v1/admin');
 
 // routes: auth, user profile, flavour, voting, flavour submission, admin (flavour managament && submissions + votes)
 
-app.use('/api/v1/consumer', consumerRoutes);
+app.use('/api/v1/default', defaultRoutes);
 app.use('/api/v1/admin', adminRoutes);
 
 app.listen(port, () => {
