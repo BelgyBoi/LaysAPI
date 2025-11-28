3d45e1bd63e50287f41aa2932ab74f96dc0df20e
Captain Jack Sparrow <tibo.kooreman.dev@gmail.com>
2025-11-28 16:05:44 +0100

    framework
    - made the v1 folder
    - made routes for default and admin
    - created script to run "npm run boot"
    - changed index.js to app.js

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const port = 3000;
 
 app.use(express.json());
 
+const defaultRoutes = require('./routes/v1/default');
+const adminRoutes = require('./routes/v1/admin')
+
 // routes: auth, user profile, flavour, voting, flavour submission, admin (flavour managament && submissions + votes)
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+app.use('/api/v1/consumer', consumerRoutes);
+app.use('/api/v1/admin', adminRoutes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
